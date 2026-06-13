cf8866ba0a9188afdfa27b41aa8a8a6e7223c356
Tor344 <per7euse@gmail.com>
2026-06-13 13:21:26 +0300

    feat: add endpoint /ping

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = FastAPI()
 async def root():
     return {"ping": "pong"}
 
+@app.get("/ping")
+async def ping():
+    return {"ping": "pong"}
+
 
 if __name__ == "__main__":
     uvicorn.run(host="0.0.0.0", port=8000,app=app)
